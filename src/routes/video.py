65c54fae4029e39e3828093bef4be54c2b44c668
Cheesypasta1 <trivedi.dhyey@yahoo.co.uk>
2025-02-02 01:12:40 +0000

    testing FE

--- src/routes/video.py
+++ src/routes/video.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-
+import boto3
 router = APIRouter()
 
+table = boto3.resource('dynamodb').Table("video")
+s3 = boto3.client('s3')
 
 @router.get("/video")
 def get_video(video_id: str):
-    pass
+    response = table.get_item(Key={"id": video_id})
+    if "Item" not in response:
+        return {"message": "Video not found"}
+    return response["Item"]
+    
